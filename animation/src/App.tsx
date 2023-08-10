91613426bf64f2659de00065a942b4a48734b9cd
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-10 15:58:34 +0900

    added switch

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import { Box, Circle, Container, Grid, Switch } from "./Styles";
+
 function App() {
-    return null;
+    const [switchActive, setSwitchActive] = useState<boolean>(false);
+
+    function handleMouseDown() {
+        console.log("handleMouseDown");
+        setSwitchActive(true);
+    }
+
+    function handleMouseUp() {
+        console.log("handleMouseUp");
+        setSwitchActive(false);
+    }
+
+    return (
+        <Container>
+            <Grid>
+                <Box></Box>
+                <Box>
+                    <Circle layoutId="dot"></Circle>
+                </Box>
+                <Box></Box>
+                <Box></Box>
+            </Grid>
+            <Switch
+                onMouseDown={handleMouseDown}
+                onMouseUp={handleMouseUp}
+                isActive={switchActive}
+            >
+                Switch
+            </Switch>
+        </Container>
+    );
 }
 
 export default App;
